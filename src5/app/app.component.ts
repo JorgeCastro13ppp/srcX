0b89f9f4afa6d992799a75a64772caf582dd94d1
JorgeCastro13ppp <jlcastroc01@educarex.es>
2023-12-15 09:11:45 +0100

    15dic

---
+++ src5/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularP8';
+
+  disminuir(){
+    this.ancho-=10;
+  }
+  aumentar(){
+    this.ancho+=10;
+  }
+  siguiente(){
+    this.indiceImagenSeleccionada++
+    if(this.indiceImagenSeleccionada==this.imagenes.length){
+      this.indiceImagenSeleccionada=0;
+    }
+    this.imagen=this.imagenes[this.indiceImagenSeleccionada];
+  }
+  anterior(){
+    this.indiceImagenSeleccionada--;
+    this.imagen=this.imagenes[this.indiceImagenSeleccionada];
+  }
+
+  imagenes = [ 'https://randomuser.me/api/portraits/women/40.jpg', 'https://randomuser.me/api/portraits/women/41.jpg', 'https://randomuser.me/api/portraits/women/42.jpg', 'https://randomuser.me/api/portraits/women/43.jpg', 'https://randomuser.me/api/portraits/women/44.jpg', 'https://randomuser.me/api/portraits/men/40.jpg', 'https://randomuser.me/api/portraits/men/41.jpg', 'https://randomuser.me/api/portraits/men/42.jpg', 'https://randomuser.me/api/portraits/men/43.jpg', 'https://randomuser.me/api/portraits/men/44.jpg'];
+  indiceImagenSeleccionada:number=0;
+  imagen:string="";
+  ancho:number=0;
+  auto:boolean=false;
+  constructor(){
+    this.indiceImagenSeleccionada=0;
+    this.ancho=300;
+    this.auto=false;
+    this.imagen=this.imagenes[this.indiceImagenSeleccionada];
+  }
+}

fad8619106e70ea4deecccb8f1cdbcd5dc243924
JorgeCastro13ppp <jlcastroc01@educarex.es>
2024-01-12 08:59:29 +0100

    crud3 bien

--- srcCrud3/app/servicio-datos.service.ts
+++ srcCrud3/app/servicio-datos.service.ts
@@ -7,14 +7,13 @@ import { Vehiculo } from './vehiculo';
   providedIn: 'root'
 })
 export class ServicioDatosService {
+  constructor(private httpCliente: HttpClient) { }
   listarVehiculos() :Observable<Vehiculo[]> {
     const url="https://random-data-api.com/api/vehicle/random_vehicle?size=100";
       return this.httpCliente.get<Vehiculo[]>(url);
   }
 
-  constructor(private httpCliente: HttpClient) {
 
-  }
 }
 
 

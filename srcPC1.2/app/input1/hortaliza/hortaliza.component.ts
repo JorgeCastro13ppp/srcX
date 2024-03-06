06de8469bb9fc102e6c7fb53ec140bfb8cf0bce2
JorgeCastro13ppp <jlcastroc01@educarex.es>
2024-03-06 10:22:28 +0100

    ffffffff

---
+++ srcPC1.2/app/input1/hortaliza/hortaliza.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-hortaliza',
+  templateUrl: './hortaliza.component.html',
+  styleUrls: ['./hortaliza.component.css']
+})
+export class HortalizaComponent {
+mensajePadre: string="";
+enviarFP() {
+
+  @Output() 
+
+}
+@Input() mensajeParaHortaliza:string=""
+}
